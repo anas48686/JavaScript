7c2984fbc3f3ae2e67a5a5c62793fc60c16db999
Mohammad Anas Hannan <mohammadanashannan@gmail.com>
2026-06-17 15:49:15 +0000

    Data Types added

---
+++ Basics/data_types.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) we are using nodejs not browser
+
+let name="Anas"
+let age =18
+let isTrue=true
+//bigint for large integers
+// null=> standalone value
+//undefined=> not assigned
+//symbol =>unique
+
+//object
+
+console.log(typeof "Anas");
+console.log(typeof 6);
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
+
+
+
