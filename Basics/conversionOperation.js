26fd56f519808bfb4bb108f095ffe8d45a7a05ec
Mohammad Anas Hannan <mohammadanashannan@gmail.com>
2026-06-22 09:14:49 +0000

    Conversion of data types

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,54 @@
+// let score="33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valInNumber=Number(score);
+// console.log(typeof valInNumber); //number
+// console.log(valInNumber); //33
+
+// score="33abc"
+// valInNumber=Number(score);
+// console.log(typeof valInNumber); //number
+// console.log(valInNumber); //NaN
+
+// score= null
+// valInNumber=Number(score);
+// console.log(typeof valInNumber); //number
+// console.log(valInNumber); //0
+
+// score=undefined
+// valInNumber=Number(score);
+// console.log(typeof valInNumber); //number
+// console.log(valInNumber); //NaN
+
+// score=true
+// valInNumber=Number(score);
+// console.log(typeof valInNumber); //number
+// console.log(valInNumber); //1
+
+// score="abc"
+// valInNumber=Number(score);
+// console.log(typeof valInNumber); //number
+// console.log(valInNumber); //NaN
+
+// "33"=>33
+//"33abc"=>NaN- Not a number
+//true=>1; false=> 0
+
+// let logIn=1
+
+// let boolLogIn=Boolean(logIn)
+// console.log(boolLogIn);
+
+// 1=>true; 0=>false
+//""=>false
+//"hitesh"=> true
+
+let someNum=33
+
+let strNum=String(someNum)
+console.log(strNum);
+console.log(typeof strNum);
+
+
